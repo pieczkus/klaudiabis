cab3847ce90a592e84f2f7fc655f66046a2eb74b
pieczkus <kudlas@gmail.com>
2014-09-21 18:04:40 +0200

    added simple breadcrumbs

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 angular.module('klaudiabis', ['ui.router', 'pascalprecht.translate', 'ngAnimate', 'navigationModule', 'languageModule'])
     .controller('AppCtrl', function HomeCtrl($scope, $state) {
         $state.go('home');
+        $scope.$state = $state;
     });
