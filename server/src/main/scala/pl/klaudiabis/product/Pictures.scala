ee3f65e1d521f11593c4f39febe61bdf5c53114a
Bartłomiej Pieczka <bartlomiej.pieczka@gmail.com>
2015-10-20 09:46:07 +0200

    some basic product management

--- server/src/main/scala/pl/klaudiabis/product/Pictures.scala
+++ server/src/main/scala/pl/klaudiabis/product/Pictures.scala
@@ -1,17 +1,5 @@
 package pl.klaudiabis.product
 
-import java.util.UUID
+class Pictures {
 
-case class Picture(id: String, bytes: Array[Byte])
-
-case class Pictures(pictures: List[Picture]) {
-
-  def withNewPicture(picture: Picture): Pictures = copy(pictures = pictures :+ picture)
-
-  def get(id: UUID): Option[Picture] = pictures.find(_.id == id)
-
-}
-
-object Pictures {
-  val empty: Pictures = Pictures(List.empty)
 }
